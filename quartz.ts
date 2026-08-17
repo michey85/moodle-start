039cf28f84553b156a78f387b2de0f9fb88392e9
Mikhail Nepomniashchii <michey85@gmail.com>
2026-08-17 18:09:26 +0200

    remove items counter

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,10 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
 
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
+
+ExternalPlugin.FolderPage({
+  showFolderCount: false,
+})
